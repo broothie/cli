1b61e62c2258890ba5d89e57d56a6ee6aadd9166
broothie <adbooth8@gmail.com>
2025-02-12 15:36:07 -0800

    more tests

--- cli_test.go
+++ cli_test.go
@@ -1,11 +1,159 @@
 package cli
 
 import (
+	"context"
 	"testing"
+
+	"github.com/samber/lo"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestRun(t *testing.T) {
-	t.Run("runs handler", func(t *testing.T) {
+func ensureCalled(t *testing.T) func() {
+	called := false
+	t.Cleanup(func() { assert.True(t, called) })
 
-	})
+	return func() { called = true }
+}
+
+func Test_git(t *testing.T) {
+	type TestCase struct {
+		rawArgs         []string
+		gitHandler      func(*testing.T) Handler
+		commitHandler   func(*testing.T) Handler
+		checkoutHandler func(*testing.T) Handler
+	}
+
+	testCases := map[string]TestCase{
+		"base handler is called": {
+			gitHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+
+				return func(ctx context.Context) error {
+					called()
+					return nil
+				}
+			},
+		},
+		"long flag is parsed": {
+			rawArgs: []string{"--git-dir", "/path/to/something"},
+			gitHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+
+					assert.Equal(t, "/path/to/something", FlagValue(ctx, "git-dir"))
+					return nil
+				}
+			},
+		},
+		"sub-command handler is called": {
+			rawArgs: []string{"commit"},
+			commitHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+					return nil
+				}
+			},
+		},
+		"flag alias is parsed": {
+			rawArgs: []string{"commit", "--msg", "a commit message"},
+			commitHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+					assert.Equal(t, "a commit message", FlagValue(ctx, "message"))
+					return nil
+				}
+			},
+		},
+		"flag short is parsed": {
+			rawArgs: []string{"commit", "-m", "a commit message"},
+			commitHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+					assert.False(t, FlagValue(ctx, "all").(bool))
+					assert.Equal(t, "a commit message", FlagValue(ctx, "message"))
+					return nil
+				}
+			},
+		},
+		"short flag group is parsed": {
+			rawArgs: []string{"commit", "-am", "a commit message"},
+			commitHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+					assert.True(t, FlagValue(ctx, "all").(bool))
+					assert.Equal(t, "a commit message", FlagValue(ctx, "message"))
+					return nil
+				}
+			},
+		},
+		"arg is parsed": {
+			rawArgs: []string{"checkout", "some-branch"},
+			checkoutHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+
+					branch, err := ArgValue(ctx, "branch")
+					assert.NoError(t, err)
+					assert.Equal(t, "some-branch", branch)
+
+					return nil
+				}
+			},
+		},
+		"bool flag is parsed with arg": {
+			rawArgs: []string{"checkout", "-b", "some-branch"},
+			checkoutHandler: func(t *testing.T) Handler {
+				called := ensureCalled(t)
+				return func(ctx context.Context) error {
+					called()
+
+					branch, err := ArgValue(ctx, "branch")
+					assert.NoError(t, err)
+					assert.Equal(t, "some-branch", branch)
+
+					assert.True(t, FlagValue(ctx, "new-branch").(bool))
+					return nil
+				}
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			command, err := NewCommand("git", "the stupid content tracker",
+				SetVersion("v0.1.0"),
+				AddFlag("git-dir", "Git directory to use"),
+				AddSubCmd("commit", "Record changes to the repository",
+					AddFlag("message", "commit message",
+						AddFlagAlias("msg"),
+						AddFlagShort('m'),
+					),
+					AddFlag("all", "commit all changed files",
+						AddFlagShort('a'),
+						SetFlagDefault(false),
+					),
+					SetHandler(lo.If(testCase.commitHandler == nil, helpHandler).ElseF(func() func(ctx context.Context) error { return testCase.commitHandler(t) })),
+				),
+				AddSubCmd("checkout", "Switch branches or restore working tree files",
+					AddArg("branch", "Branch to check out"),
+					AddFlag("new-branch", "New branch name",
+						AddFlagShort('b'),
+						SetFlagDefault(false),
+					),
+					SetHandler(lo.If(testCase.checkoutHandler == nil, helpHandler).ElseF(func() func(ctx context.Context) error { return testCase.checkoutHandler(t) })),
+				),
+				SetHandler(lo.If(testCase.gitHandler == nil, helpHandler).ElseF(func() func(ctx context.Context) error { return testCase.gitHandler(t) })),
+			)
+
+			require.NoError(t, err)
+			assert.NoError(t, command.Run(context.TODO(), testCase.rawArgs))
+		})
+	}
 }
