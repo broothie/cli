e5f9efa5e59f9eb40af028797e88537c8a35012f
broothie <adbooth8@gmail.com>
2025-02-11 20:29:16 -0800

    add command test

---
+++ command_test.go
@@ -0,0 +1,72 @@
+package cli
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCommand_root(t *testing.T) {
+	command, err := New("root", "", AddSubCmd("child", ""))
+	require.NoError(t, err)
+
+	require.NotEmpty(t, command.subCommands)
+	subCommand := command.subCommands[0]
+
+	assert.True(t, command.isRoot())
+	assert.False(t, subCommand.isRoot())
+}
+
+func TestCommand_hasParent(t *testing.T) {
+	command, err := New("root", "", AddSubCmd("child", ""))
+	require.NoError(t, err)
+
+	require.NotEmpty(t, command.subCommands)
+	subCommand := command.subCommands[0]
+
+	assert.False(t, command.hasParent())
+	assert.True(t, subCommand.hasParent())
+}
+
+func TestCommand_qualifiedName(t *testing.T) {
+	command, err := New("root", "",
+		AddSubCmd("child", "",
+			AddSubCmd("grandchild", ""),
+		),
+	)
+	require.NoError(t, err)
+
+	require.NotEmpty(t, command.subCommands)
+	childCommand := command.subCommands[0]
+
+	require.NotEmpty(t, childCommand.subCommands)
+	grandChildCommand := childCommand.subCommands[0]
+
+	assert.Equal(t, command.qualifiedName(), "root")
+	assert.Equal(t, childCommand.qualifiedName(), "root child")
+	assert.Equal(t, grandChildCommand.qualifiedName(), "root child grandchild")
+}
+
+func TestCommand_findVersion(t *testing.T) {
+	command, err := New("root", "",
+		SetVersion("v0.1.0"),
+		AddSubCmd("child", "",
+			AddSubCmd("grandchild", "",
+				SetVersion("v0.3.0"),
+			),
+		),
+	)
+
+	assert.NoError(t, err)
+
+	require.NotEmpty(t, command.subCommands)
+	childCommand := command.subCommands[0]
+
+	require.NotEmpty(t, childCommand.subCommands)
+	grandChildCommand := childCommand.subCommands[0]
+
+	assert.Equal(t, command.findVersion(), "v0.1.0")
+	assert.Equal(t, childCommand.findVersion(), "v0.1.0")
+	assert.Equal(t, grandChildCommand.findVersion(), "v0.3.0")
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,12 @@ require (
 	github.com/bobg/errors v1.1.0
 	github.com/broothie/option v0.1.0
 	github.com/samber/lo v1.49.1
+	github.com/stretchr/testify v1.10.0
 )
 
-require golang.org/x/text v0.21.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
