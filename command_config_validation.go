c67602550d0199616bf1cb6001074ee5d5727071
broothie <adbooth8@gmail.com>
2025-02-15 18:20:21 -0800

    validate duplicate flags and args

--- command_config_validation.go
+++ command_config_validation.go
@@ -6,6 +6,8 @@ import (
 
 func (c *Command) validateConfig() error {
 	validations := []func() error{
+		c.validateNoDuplicateFlags,
+		c.validateNoDuplicateArguments,
 		c.validateNoDuplicateSubCommands,
 		c.validateEitherCommandsOrArguments,
 	}
@@ -18,16 +20,48 @@ func (c *Command) validateConfig() error {
 	return errors.Join(errs...)
 }
 
+func (c *Command) validateNoDuplicateFlags() error {
+	flags := make(map[string]bool)
+
+	var errs []error
+	for _, flag := range c.flags {
+		for _, name := range append([]string{flag.name}, flag.aliases...) {
+			if flags[name] {
+				errs = append(errs, errors.Errorf("duplicate flag %q", name))
+			}
+
+			flags[name] = true
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+func (c *Command) validateNoDuplicateArguments() error {
+	arguments := make(map[string]bool)
+
+	var errs []error
+	for _, argument := range c.arguments {
+		if arguments[argument.name] {
+			errs = append(errs, errors.Errorf("duplicate argument %q", argument.name))
+		}
+
+		arguments[argument.name] = true
+	}
+
+	return errors.Join(errs...)
+}
+
 func (c *Command) validateNoDuplicateSubCommands() error {
-	set := make(map[string]bool)
+	commands := make(map[string]bool)
 
 	var errs []error
 	for _, command := range c.subCommands {
-		if set[command.name] {
+		if commands[command.name] {
 			errs = append(errs, errors.Errorf("duplicate sub-command %q", command.name))
-		} else {
-			set[command.name] = true
 		}
+
+		commands[command.name] = true
 	}
 
 	return errors.Join(errs...)
