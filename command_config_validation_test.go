8b0b60425ae6940d7011c2273e8efaebf9444c53
broothie <adbooth8@gmail.com>
2025-02-16 17:02:19 -0800

    add command config validations

---
+++ command_config_validation_test.go
@@ -0,0 +1,53 @@
+package cli
+
+import (
+	"testing"
+
+	"github.com/broothie/option"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommand_config_validations(t *testing.T) {
+	type TestCase struct {
+		commandOptions option.Options[*Command]
+		expectedError  string
+	}
+
+	testCases := map[string]TestCase{
+		"validateNoDuplicateFlags": {
+			commandOptions: option.NewOptions(
+				AddFlag("some-flag", "some flag"),
+				AddFlag("some-flag", "some flag"),
+			),
+			expectedError: `invalid command "test": duplicate flag "some-flag"`,
+		},
+		"validateNoDuplicateArguments": {
+			commandOptions: option.NewOptions(
+				AddArg("some-arg", "some arg"),
+				AddArg("some-arg", "some arg"),
+			),
+			expectedError: `invalid command "test": duplicate argument "some-arg"`,
+		},
+		"validateNoDuplicateSubCommands": {
+			commandOptions: option.NewOptions(
+				AddSubCmd("some-sub-command", "some sub-command"),
+				AddSubCmd("some-sub-command", "some sub-command"),
+			),
+			expectedError: `invalid command "test": duplicate sub-command "some-sub-command"`,
+		},
+		"validateEitherCommandsOrArguments": {
+			commandOptions: option.NewOptions(
+				AddArg("some-arg", "some arg"),
+				AddSubCmd("some-sub-command", "some sub-command"),
+			),
+			expectedError: `invalid command "test": cannot have both sub-commands and arguments`,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			_, err := NewCommand("test", "test command", testCase.commandOptions...)
+			assert.EqualError(t, err, testCase.expectedError)
+		})
+	}
+}
